343010637aa00e5a04d2c485e48267150293ba68
Cherry <222677431+Cherry-Cerise@users.noreply.github.com>
2025-08-26 04:35:56 +0200

    Solve "09_sumAll" exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof(num1) != "number" || typeof(num2) != "number" || Math.floor(num1) != num1 ||  Math.floor(num2) != num2 || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (num2 < num1) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
