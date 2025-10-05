18b2d50a60bdf08dfd77e1aec7cb2e531f74e71e
Cherry <222677431+Cherry-Cerise@users.noreply.github.com>
2025-10-05 21:32:36 +0200

    Solve 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = [];
+    for(let i = 0; i < string.length; i++) {
+        if(string[i] != " " && string[i] != "." && string[i] != "," && string[i] != "!" && string[i] != "?") {
+            arr.push(string[i].toLowerCase());
+        }
+    }
+    return arr.join("") === arr.reverse().join("");
 };
 
 // Do not edit below this line
