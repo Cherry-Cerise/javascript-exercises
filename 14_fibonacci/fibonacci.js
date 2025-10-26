1401384b343f6936c6b43dbd0ece44ac15228d22
Cherry <222677431+Cherry-Cerise@users.noreply.github.com>
2025-10-26 14:37:48 +0100

    Solve "14_fibonacci" exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthNumber) {
+    nthNumber = +nthNumber;
+    if(nthNumber < 0) return "OOPS";
+    const sequence = [0, 1,];
+    for(i = 2; i <= nthNumber; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence[nthNumber];
 };
 
 // Do not edit below this line
