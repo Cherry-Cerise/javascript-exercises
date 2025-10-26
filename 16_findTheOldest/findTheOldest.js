31f90cbd7a887ae08d3e1c5247b91ee0205cc981
Cherry <222677431+Cherry-Cerise@users.noreply.github.com>
2025-10-26 16:13:40 +0100

    Solve "16_findTheOldest" exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    let sortedArray = peopleArray.sort((a, b) => {
+        a.yearOfDeath ??= new Date().getFullYear();
+        b.yearOfDeath ??= new Date().getFullYear();
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    });
+    return sortedArray[0];
 };
 
 // Do not edit below this line
